41c7126d5dd9662e0a290a6f52e947f1ba3b9247
amitthakur2013 <amitthakur2013@gmail.com>
2021-05-17 15:51:42 +0530

    route handler added to support bill payment

--- src/main/java/com/springboot/bankapp/controller/VendorController.java
+++ src/main/java/com/springboot/bankapp/controller/VendorController.java
@@ -1,6 +1,10 @@
 package com.springboot.bankapp.controller;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +14,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.bankapp.entity.User;
 import com.springboot.bankapp.entity.Vendor;
+import com.springboot.bankapp.service.UserService;
 import com.springboot.bankapp.service.VendorService;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/banking/payments")
 public class VendorController {
+	
+	@Autowired
+	private UserService userService;
+	
 	@Autowired
 	private VendorService vendorService;
 	
@@ -29,6 +39,26 @@ public class VendorController {
 	public ResponseEntity<?> vendorDetails(@PathVariable long id){
 		try {
 			return ResponseEntity.ok(this.vendorService.getVendorById(id));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.ok(e.getMessage());
+		}
+	}
+	
+	@GetMapping("/vendors/user")
+	public ResponseEntity<?> userVendors(Principal principal){
+		try {
+			String username=principal.getName();
+			User user=this.userService.findByUserName(username);
+			Set<List> vendorsList = new HashSet<List>();
+			user.getBillerList().forEach((biller)->{
+				List a=new ArrayList<>();
+				a.add(biller.getVendor().getVcompName());
+				a.add(biller.getVendor().getCategory());
+				vendorsList.add(a);
+			});
+			return ResponseEntity.ok(vendorsList);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			return ResponseEntity.ok(e.getMessage());
